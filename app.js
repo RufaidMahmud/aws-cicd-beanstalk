eae18f009190979f4dc56c1ecf7c887f62953a43
Rufaid Mahmud <rufaidmhmud@gmai.com>
2026-08-11 19:15:10 +0600

    Serve portfolio through Express

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.static(__dirname));
+
 app.get("/", (req, res) => {
-    res.send("Hello from AWS CI/CD!");
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
